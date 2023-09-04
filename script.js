0c5ebd5606242b8ee92c52af5af765a8c360605f
hajar746 <132266440+hajar746@users.noreply.github.com>
2023-09-04 17:24:29 +0300

    add a sticky nav but page is jumping when scrolling

--- script.js
+++ script.js
@@ -2,8 +2,25 @@
 
 // responsive nav
 const hamburger = document.querySelector(".hamburger");
-const nav = document.querySelector(".nav_links");
+const navLinks = document.querySelector(".nav_links");
 
 hamburger.addEventListener("click", function () {
-  nav.classList.toggle("active");
+  navLinks.classList.toggle("active");
 });
+
+// sticky nav (using intersection observer API)
+// selecting header & nav
+const headerNav = document.querySelector("header");
+const nav = document.querySelector(".nav");
+// add sticky class when the header is not intersecting the viewport
+const stickyNav = function (entries) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) nav.classList.add("sticky");
+  else nav.classList.remove("sticky");
+};
+const headerObserve = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+});
+// selecting the element to be observed (the header navbar)
+headerObserve.observe(headerNav);

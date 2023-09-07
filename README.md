613f2c9492a7902173cf451224c901d8a6c0a595
hajar746 <132266440+hajar746@users.noreply.github.com>
2023-09-07 16:16:27 +0300

    add slide implementation to latest recipe section using js

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ basic recipe website
 use basic html to make this website
 
 Arrow by Flaticon from Creartive
-Dessert icons created by smashingstocks - Flaticon
+Dessert and Fork icons created by smashingstocks - Flaticon
--- script.js
+++ script.js
@@ -24,3 +24,110 @@ const headerObserve = new IntersectionObserver(stickyNav, {
 });
 // selecting the element to be observed (the header navbar)
 headerObserve.observe(headerNav);
+
+// slider component
+const slider = function () {
+  const slides = document.querySelectorAll(".slide");
+  const btnRight = document.querySelector(".slide-btn--right");
+  const btnLeft = document.querySelector(".slide-btn--left");
+
+  // setting current slide to 0
+  let curSlide = 0;
+  const maxSlide = slides.length - 1;
+
+  // Functions
+  // function to go change transform style of slides when arrows are pressed
+  const goToSlide = function (slide) {
+    slides.forEach(
+      (s, i) => (s.style.transform = `translateX(${600 * (i - slide)}%)`)
+    );
+  };
+
+  // function to go to next slide
+  const nextSlide = function () {
+    if (curSlide === maxSlide) {
+      curSlide = 0;
+    } else curSlide++;
+    goToSlide(curSlide);
+    activateDot(curSlide);
+  };
+
+  // function to go to previous slide
+  const prevSlide = function () {
+    if (curSlide === 0) {
+      curSlide = maxSlide;
+    } else curSlide--;
+    goToSlide(curSlide);
+    activateDot(curSlide);
+  };
+
+  // function for creating a dot for each slide
+  const dotsContainer = document.querySelector(".dots");
+  const createDots = function () {
+    slides.forEach((_, i) => {
+      dotsContainer.insertAdjacentHTML(
+        "beforeend",
+        `<button class="dots-dot" data-slide='${i}'></button>`
+      );
+    });
+  };
+
+  // change color of dot when active
+  const activateDot = function (slide) {
+    // removing all active classes
+    document
+      .querySelectorAll(".dots-dot")
+      .forEach((dot) => dot.classList.remove("dots-dot--active"));
+    // adding active class
+    document
+      .querySelector(`.dots-dot[data-slide='${slide}']`)
+      .classList.add("dots-dot--active");
+  };
+
+  // initialization function
+  const init = function () {
+    // set original position of slides
+    goToSlide(0);
+    // create dots
+    createDots();
+    // activate first dot
+    activateDot(0);
+  };
+  init();
+
+  // Event handlers
+  // go to next and previous slides
+  btnRight.addEventListener("click", nextSlide);
+  btnLeft.addEventListener("click", prevSlide);
+
+  // sliding using dots
+  dotsContainer.addEventListener("click", function (e) {
+    if (e.target.classList.contains("dots-dot")) {
+      curSlide = Number(e.target.dataset.slide);
+      goToSlide(curSlide);
+      activateDot(curSlide);
+    }
+  });
+};
+
+// Revealing sections on scroll
+const sections = document.querySelectorAll(".section");
+//removing hidden class from sections
+const revealSec = function (entries, observer) {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) return;
+    else entry.target.classList.remove("section--hidden");
+    // stop observing sections after they are revealed
+    observer.unobserve(entry.target);
+  });
+};
+
+const sectionObserver = new IntersectionObserver(revealSec, {
+  root: null,
+  threshold: 0.1,
+});
+// observing each section and hiding it
+sections.forEach((section) => {
+  sectionObserver.observe(section);
+  section.classList.add("section--hidden");
+});
